bebdc2fb7ae89d7a7c2dd4ca9f3e125e11ce10df
team-rescue <36742931+team-rescue@users.noreply.github.com>
2018-03-22 08:13:43 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE  html>
+<html>
+<head>
+	<title>Table with database</title>
+</head>
+<body>
+<table>
+<tr>
+	<th>id</th>
+	<th>username</th>
+	<th>password</th>
+</tr>
+<?php
+$conn = mysqli_connect("localhost","root","","test");
+if($conn->connect_error){
+	 die( "Unable to select database". $connect_error);
+}
+$sql="SELECT id,username,password FROM mentor";
+$result=$conn->query($sql);
+if($result-> num_rows > 0){
+	while($row=$result-> fetch_assoc()){
+		echo "<tr><td>".$row["id"] ."</td><td>" .$row["username"] ."</td><td>". $row["password"] ."</td></tr>" ; 
+	}
+	echo"</table>";
+}
+else{
+	echo "0 result";
+}
+$conn-> close();
+?>
+</table>
+</body>
+</html>
